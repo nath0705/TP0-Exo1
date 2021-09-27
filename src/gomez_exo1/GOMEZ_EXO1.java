e2106e707710459b5b68e08993130bf30ebcfa11
Natha <Natha@DESKTOP-IMFIRBI>
2021-09-27 12:54:57 +0200

    Premiere version

---
+++ src/gomez_exo1/GOMEZ_EXO1.java
@@ -0,0 +1,36 @@
+/* GOMEZ Nathanael
+   TP0
+   Exo 1
+   27 septembre 
+*/
+
+package gomez_exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nathanael Gomez
+ */
+public class GOMEZ_EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        int p = 22;
+        
+        p=p+3;
+        System.out.println("Au revoir");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, est ce que ça va?");
+        prenom = sc.nextLine();
+    }
+    
+    
+
+}
